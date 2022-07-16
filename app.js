7519c7b44ed5689c4fcfcb84e29b88ee33b155ba
Luis Rosales <luismrosales2@gmail.com>
2022-07-16 00:45:48 -0400

    added api fetch

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // api key  563492ad6f9170000100000133a62dce9eaf433ba84252c29d6fb592
+
+const auth = "563492ad6f9170000100000133a62dce9eaf433ba84252c29d6fb592";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+let searchValue;
+
+// Event Listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: { Accept: "application/json", Authorization: auth },
+  });
+  const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>
+        `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function curatedPhotos() {
+  const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15");
+  generatePictures(data);
+}
+
+async function searchPhotos(search) {
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${search}+query&per_page=15`
+  );
+  generatePictures(data);
+}
+curatedPhotos();
